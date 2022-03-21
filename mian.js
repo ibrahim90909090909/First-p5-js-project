ba62d8842d5a45067b5fb4551561550d314b6dbc
ibrahim90909090909 <96910035+ibrahim90909090909@users.noreply.github.com>
2022-03-21 11:55:22 +0000

    Add files via upload

---
+++ mian.js
@@ -0,0 +1,13 @@
+function preload(){
+}
+function setup(){
+canvas = createCanvas(680, 480);
+canvas.position(110, 250);
+}
+function draw(){
+image(img, 10, 10, 150, 120,);
+
+Fill(0, 128, 0);
+stroke(0, 128, 0);
+circle(350, 50, 70);
+}
